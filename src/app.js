055566fbab9a409d849b1a606ef706189f858bf4
Ahmed F Gadir <e17769@hotmail.com>
2023-12-19 07:22:21 -0500

    First look

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-var http = require('http'),
-  fs = require('fs');
-
-fs.readFile('./src/index.html', function (err, html) {
-  if (err) {
-    throw err;
-  }
-  http
-    .createServer(function (request, response) {
-      response.writeHeader(200, { 'Content-Type': 'text/html' });
-      response.write(html);
-      response.end();
-    })
-    .listen(8001);
-});
+var http = require('http');
+var fs = require('fs');
+http
+  .createServer(function (req, res) {
+    //Open a file on the server and return its content:
+    fs.readFile('./src/index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8080/');
 console.log('Click Ctrl+C to stop');
---
+++ src/gemini.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementById('maingMsg').innerHTML = 'Paragraph changed.';
+}
